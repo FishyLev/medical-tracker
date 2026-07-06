e4d493ce545a1af6a349a43c611e57937d2510b6
FishyLev <fishylevaithan@gmail.com>
2026-07-06 11:26:49 +0530

    Fix Netlify CORS origin

--- app/main.py
+++ app/main.py
@@ -20,13 +20,13 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://medical-assistant-v3.netlify.app/",
-],
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://medical-assistant-v3.netlify.app",
+    ],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allow_headers=["Authorization", "Content-Type", "Accept", "Origin"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
